b113e059cbafc26b2ccc2738dea945def5c07d0a
eaparicio45 <erapariciopuerta@hotmail.com>
2017-06-06 14:16:26 +0200

    finished srnaDE form (views needs to be fixed)

--- sRNAtoolboxweb/sRNAde/forms.py
+++ sRNAtoolboxweb/sRNAde/forms.py
@@ -10,7 +10,13 @@ from progress.models import JobStatus
 from sRNAtoolboxweb.settings import BASE_DIR
 from sRNAtoolboxweb.settings import MEDIA_ROOT
 from utils.pipeline_utils import generate_uniq_id
+from utils.sysUtils import *
 
+def check_mat_file(mat):
+    if istext(mat) and istabfile(mat):
+        return True
+    else:
+        return False
 
 class DEForm(forms.Form):
     lisfofIDs = forms.CharField(label='List of sRNAbench IDs (colon separated, separate groups by hashes):',
@@ -22,22 +28,44 @@ class DEForm(forms.Form):
     ifile = forms.FileField(label='Or upload a matrix of expression values:', required=False)
     sampleGroups = forms.CharField(label='Sample groups (hash separated):',
                              required=False, widget=forms.TextInput(attrs={'placeholder': "e.g: Normal#TumorI#TumorII"}))
-    sampleDescription = forms.CharField(label='Sample description (colon separated):',
+    matDescription = forms.CharField(label='Sample description (colon separated):',
                              required=False, widget=forms.TextInput(attrs={'placeholder': "e.g: Normal:Normal:TumorI:TumorI:TumorII:TumorII:TumorII"}))
     pvalue= forms.CharField(label='Differential expression cutoff used by DESeq and EdgeR (p-value):', default= "0.05",
                              required=False, widget=forms.TextInput(attrs={'placeholder': "Default 0.05"}))
     probability = forms.CharField(label='Differential expression cutoff used by NOISeq(probability):', default= "0.8",
-                             required=False, widget=forms.TextInput(attrs={'placeholder': "Default 0.05"}))
+                             required=False, widget=forms.TextInput(attrs={'placeholder': "Default 0.8"}))
     isomiRs = forms.BooleanField(label='isoMir Analysis', required=False )
 
     def clean(self):
         cleaned_data = super(DEForm, self).clean()
-        if not cleaned_data.get('ifile') and not cleaned_data.get('url'):
+        if not cleaned_data.get('ifile') and not cleaned_data.get('listofIDs'):
             self.add_error('ifile', 'One of these two fields is required')
-            self.add_error('url', 'One of these two fields is required')
-        if cleaned_data.get('ifile') and cleaned_data.get('url'):
-            self.add_error('ifile', 'Choose either file or URL')
-            self.add_error('url', 'Choose either file or URL')
+            self.add_error('listofIDs', 'One of these two fields is required')
+        if cleaned_data.get('ifile') and cleaned_data.get('listofIDs'):
+            self.add_error('ifile', 'Choose either List of IDs or matrix expression file')
+            self.add_error('listofIDs', 'Choose either List of IDs or matrix expression file')
+        if cleaned_data.get('ifile') and not cleaned_data.get('matDescription'):
+            self.add_error('matDescription', 'This field is mandatory if a matrix of Expression Values is provided ')
+        if cleaned_data.get('listofIDs') and not cleaned_data.get('sampleGroups'):
+            self.add_error('matDescription', 'This field is mandatory if a list of sRNAbench IDs is provided')
+        if (1.0> float(cleaned_data.get('pvalue'))) and (0.0 < float(cleaned_data.get('pvalue'))):
+            self.add_error('pvalue', 'Must be a number between 1 and 0')
+        if (1.0> float(cleaned_data.get('probability'))) and (0.0 < float(cleaned_data.get('probability'))):
+            self.add_error('probability', 'Must be a number between 1 and 0')
+
+        if  cleaned_data.get('listofIDs'):
+            samples = len(cleaned_data.get('listofIDs').split("#"))
+            groups = len(cleaned_data.get('sampleGroups').split("#"))
+            if not (groups==samples):
+                self.add_error('listofIDs', 'Number of groups must be the same, separate with #')
+                self.add_error('sampleGroups', 'Number of groups must be the same, separate with #')
+            if cleaned_data.get('matDescription'):
+                desc= len(cleaned_data.get('matDescription').split("#"))
+                if not (groups==samples==desc):
+                    self.add_error('listofIDs', 'Number of groups must be the same, separate with #')
+                    self.add_error('sampleGroups', 'Number of groups must be the same, separate with #')
+                    self.add_error('matDescription', 'Number of groups must be the same, separate with #')
+
         return cleaned_data
 
     def generate_id(self):
@@ -49,6 +77,10 @@ class DEForm(forms.Form):
 
     def create_call(self):
 
+        if self.cleaned_data.get("isomiRs"):
+            isom= "true"
+        else:
+            isom="false"
         pipeline_id = self.generate_id()
         FS = FileSystemStorage()
         FS.location = os.path.join(MEDIA_ROOT, pipeline_id)
@@ -59,31 +91,56 @@ class DEForm(forms.Form):
             file_to_update = ifile
             uploaded_file = str(file_to_update)
             ifile = FS.save(uploaded_file, file_to_update)
-        elif self.cleaned_data.get("url"):
-            url_input = self.cleaned_data.get("url")
-            dest = os.path.join(FS.location, os.path.basename(url_input))
-            handler = urllib.URLopener()
-            handler.retrieve(url_input, dest)
-            ifile = dest
-
-        else:
-            raise Http404
-
-        name = pipeline_id + '_h_extract'
-        JobStatus.objects.create(job_name=name, pipeline_key=pipeline_id, job_status="not launched",
-                                 start_time=datetime.datetime.now(),
-                                 #finish_time=datetime.time(0, 0),
-                                 all_files=ifile,
-                                 modules_files="",
-                                 pipeline_type="helper",
-                                )
-
-        return 'qsub -v pipeline="helper",mode="extract",key="{pipeline_id}",outdir="{out_dir}",inputfile="{input_file}",string="{string}",name="{name}" -N {job_name} {sh}'.format(
+            if not check_mat_file(ifile):
+                raise Http404
+            name = pipeline_id + '_de'
+            JobStatus.objects.create(job_name=name, pipeline_key=pipeline_id, job_status="not launched",
+                                     start_time=datetime.datetime.now(),
+                                     # finish_time=datetime.time(0, 0),
+                                     all_files=ifile,
+                                     modules_files="",
+                                     pipeline_type="de",
+                                     )
+            return 'qsub -v pipeline="de",matrixDesc={input_file},key="{pipeline_id}",outdir="{out_dir}",iso={iso},nt={probab},dt={pval},hmTop={top},hmPerc={perc},input="{input_file}",name="{name}" -N {job_name} {sh}'.format(
                 pipeline_id=pipeline_id,
                 out_dir=out_dir,
                 input_file=os.path.join(FS.location, ifile),
                 string=self.cleaned_data.get("string"),
                 name=name,
                 job_name=name,
-                sh=os.path.join(BASE_DIR + '/core/bash_scripts/run_helper_extract.sh')
+                probab=self.cleaned_data.get("probability"),
+                pval=self.cleaned_data.get("pvalue"),
+                top="20",
+                perc="1",
+                iso=isom,
+                sh=os.path.join(BASE_DIR + '/core/bash_scripts/run_sRNAde_matrix.sh')
             )
+        elif self.cleaned_data.get("lisfofIDs"):
+            input = self.cleaned_data.get("lisfofIDs")
+            groups = self.cleaned_data.get("sampleGroups")
+            name = pipeline_id + '_de'
+            JobStatus.objects.create(job_name=name, pipeline_key=pipeline_id, job_status="not launched",
+                                     start_time=datetime.datetime.now(),
+                                     # finish_time=datetime.time(0, 0),
+                                     all_files=ifile,
+                                     modules_files="",
+                                     pipeline_type="de",
+                                     )
+            return 'qsub -v pipeline="de",group={groups},key="{pipeline_id}",outdir="{out_dir}",iso={iso},nt={probab},dt={pval},hmTop={top},hmPerc={perc},input="{input_file}",name="{name}" -N {job_name} {sh}'.format(
+                pipeline_id=pipeline_id,
+                out_dir=out_dir,
+                group=groups,
+                matdesc= self.cleaned_data.get("matDescription"),
+                input_file=os.path.join(FS.location, ifile),
+                string=self.cleaned_data.get("string"),
+                name=name,
+                job_name=name,
+                probab=self.cleaned_data.get("probability"),
+                pval=self.cleaned_data.get("pvalue"),
+                top="20",
+                perc="1",
+                iso=self.cleaned_data.get("isomiRs"),
+                sh=os.path.join(BASE_DIR + '/core/bash_scripts/run_sRNAde.sh')
+
+        else:
+            raise Http404
