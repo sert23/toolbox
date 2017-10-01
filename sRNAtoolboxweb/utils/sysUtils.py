2d91853ded5566880b5afaa8a22c813be82a3b0c
antonior26 <aruemar@gmail.com>
2017-10-01 23:02:16 +0100

    Change library to detect file type in order to avoid errors with some files

--- sRNAtoolboxweb/utils/sysUtils.py
+++ sRNAtoolboxweb/utils/sysUtils.py
@@ -1,6 +1,6 @@
 
 import os
-import mimetypes
+import magic
 
 __author__ = 'antonior'
 
@@ -13,8 +13,9 @@ def make_dir(path):
 
 
 def istext(filename):
-    mime = mimetypes.guess_type(filename)[0]
-    if mime is None or 'text' in mime:
+    f = magic.Magic(mime=True)
+    mime = f.from_file(filename=filename)
+    if 'text' in mime:
         return True
     return False
 
