92f22dd274ddde5d7485e939d5be009efba554fb
antonior26 <aruemar@gmail.com>
2017-10-04 18:41:28 +0100

    fixed java command

--- core/pipelines/tools/sRNAdePipeline.py
+++ core/pipelines/tools/sRNAdePipeline.py
@@ -78,7 +78,6 @@ class sRNAdePipeline(Pipeline):
         else:
             cmd = "java -jar " + self.configuration.path_to_makede + " input=" + self.input + " iso=" + self.iso + " hmTop=" + self.hmt + " hmPerc=" + self.hmp + " fdr=" + self.dt + " noiseq=" + self.nt + " matrixDesc=" + self.md.replace(":", ",") + " output=" + self.outdir + " minRCexpr=5 "#rscripts=/shared/sRNAtoolbox/rscripts"
 
-        cmd="testing"
         self.set_java_command_line(cmd)
         os.system(cmd)
 
