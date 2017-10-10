318d7bdf9af6a8546585b5fc1c38eaa81561a692
eaparicio45 <erapariciopuerta@hotmail.com>
2017-10-10 10:00:55 +0200

    changing the form

--- core/pipelines/tools/sRNAdePipeline.py
+++ core/pipelines/tools/sRNAdePipeline.py
@@ -79,6 +79,7 @@ class sRNAdePipeline(Pipeline):
             cmd = "java -jar " + self.configuration.path_to_makede + " input=" + self.input + " iso=" + self.iso + " hmTop=" + self.hmt + " hmPerc=" + self.hmp + " fdr=" + self.dt + " noiseq=" + self.nt + " matrixDesc=" + self.md.replace(":", ",") + " output=" + self.outdir + " minRCexpr=5 "#rscripts=/shared/sRNAtoolbox/rscripts"
 
         self.set_java_command_line(cmd)
+        print(cmd)
         os.system(cmd)
 
 
