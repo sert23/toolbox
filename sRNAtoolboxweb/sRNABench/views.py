8c2c6366f2874b60ef0d19c984772368dec8aea5
eaparicio45 <erapariciopuerta@hotmail.com>
2018-02-16 18:11:13 +0100

    fixing hairpin (mirBase) table

--- sRNAtoolboxweb/sRNABench/views.py
+++ sRNAtoolboxweb/sRNABench/views.py
@@ -517,7 +517,8 @@ def render_table(request, mode, job_id, lib=""):
     if mode == "pre-microRNA":
         result["title"] = "Mapping results to pre-microRNA"
         ifile = os.path.join(new_record.outdir, "hairpin_sense_SA.grouped")
-        parser = MirBaseParser(ifile)
+        #parser = MirBaseParser(ifile)
+        parser = MatureParser(ifile)
         table = [obj for obj in parser.parse()]
         id = "table"
         try:
