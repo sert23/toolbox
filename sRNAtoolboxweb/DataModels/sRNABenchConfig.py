eb8ac98fa20a3888b83a978c1ae22acdc33fb0f2
eaparicio45 <erapariciopuerta@hotmail.com>
2018-02-16 13:16:30 +0100

    removing assembly from sRNAbench config

--- sRNAtoolboxweb/DataModels/sRNABenchConfig.py
+++ sRNAtoolboxweb/DataModels/sRNABenchConfig.py
@@ -104,6 +104,7 @@ class SRNABenchConfig():
             if attr== "assembly":
                 print((self.__dict__[attr]))
                 delattr(self, attr)
+                continue
             if attr == "species":
                 fdw.write("species" + "=" + ":".join(self.__dict__[attr]) + "\n")
             elif attr != "libs" and attr != "desc":
