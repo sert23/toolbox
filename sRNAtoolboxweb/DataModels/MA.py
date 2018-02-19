4c37fb445d0e880f7f4a37af2c1090858df8a2fa
eaparicio45 <erapariciopuerta@hotmail.com>
2018-02-19 20:39:46 +0100

    displaying libs tab

--- sRNAtoolboxweb/DataModels/MA.py
+++ sRNAtoolboxweb/DataModels/MA.py
@@ -1,7 +1,7 @@
 __author__ = 'antonior'
 
 class MA():
-    def __init__(self, name, unique_reads,	read_count, read_count_mult_map_adj_, RPM_lib, RPM_total, chromosomeString="--"):
+    def __init__(self, name, unique_reads,	read_count, read_count_mult_map_adj_, RPM_lib, RPM_total, chromosomeString="--",*args, **kwargs):
         self.chromosomeString = chromosomeString
         self.RPM_total = round(float(RPM_total), 2)
         self.RPM_lib = round(float(RPM_lib), 2)
