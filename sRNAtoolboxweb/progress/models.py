4415ca9fe1adefb154eeefcf85d86eeec9e940f5
eaparicio45 <erapariciopuerta@hotmail.com>
2018-02-20 15:41:49 +0100

    adding sRNAde to installed apps

--- sRNAtoolboxweb/progress/models.py
+++ sRNAtoolboxweb/progress/models.py
@@ -9,7 +9,7 @@ class JobStatus (models.Model):
     job_status = models.CharField(max_length=100)
     start_time = models.DateTimeField(default=0)
     finish_time = models.DateTimeField(null=True)
-    all_files = models.CharField(max_length=1000)
+    all_files = models.CharField(max_length=1000, null=True)
     modules_files = models.CharField(max_length=1000)
     pipeline_type = models.CharField(max_length=100)
     heatmaps = models.CharField(max_length=1000, null=True)
