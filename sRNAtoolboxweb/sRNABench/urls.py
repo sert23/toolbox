e51984946c81c5bdb1d77ecb339d34b054b4feda
eaparicio45 <erapariciopuerta@hotmail.com>
2018-02-20 10:31:32 +0100

    fixing show_align

--- sRNAtoolboxweb/sRNABench/urls.py
+++ sRNAtoolboxweb/sRNABench/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'results', views.result_new, name='srnabench'),
     #url(r'results', views.result),
     url(r'table/(.+)/(.+)/(.+)*', views.render_table, name='table_bench'),
-    url(r'align/(.+)/(.+)/(.+)', views.show_align, name='table_align'),
+    url(r'align/(.+)/(.+)/(.+)', views.show_align, name='show_align'),
     # url(r'run$', views.run, name='run_bench'),
     url(r'^$', Bench.as_view(),  name="BENCH"),
     url(r'test$', views.test),
