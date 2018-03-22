99fb91739388d8421319050da44120d1c085e019
eaparicio45 <erapariciopuerta@hotmail.com>
2018-03-22 16:34:34 +0100

    Trying to validate before uploading

--- sRNAtoolboxweb/sRNABench/views.py
+++ sRNAtoolboxweb/sRNABench/views.py
@@ -723,18 +723,22 @@ class Bench(FormView):
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        call, pipeline_id = form.create_call()
-        self.success_url = reverse_lazy('srnabench') + '?id=' + pipeline_id
 
-        print(call)
-        os.system(call)
-        js = JobStatus.objects.get(pipeline_key=pipeline_id)
-        js.status.create(status_progress='sent_to_queue')
-        js.job_status = 'sent_to_queue'
-        js.save()
+        #form.clean()
+
+        #call, pipeline_id = form.create_call()
+        #self.success_url = reverse_lazy('srnabench') + '?id=' + pipeline_id
+
+        #print(call)
+        #os.system(call)
+        #js = JobStatus.objects.get(pipeline_key=pipeline_id)
+        #js.status.create(status_progress='sent_to_queue')
+        #js.job_status = 'sent_to_queue'
+        #js.save()
         return super(Bench, self).form_valid(form)
 
-
+    def print_file_locat(self, form):
+        print(form.cleaned_data)
 
 
 
