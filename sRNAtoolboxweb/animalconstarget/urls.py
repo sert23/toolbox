51af9f8a8afe0e04962c170cf5479a5d4748a4e8
eaparicio45 <erapariciopuerta@hotmail.com>
2018-04-03 13:19:14 +0200

    Fixing results amirconstarget

--- sRNAtoolboxweb/animalconstarget/urls.py
+++ sRNAtoolboxweb/animalconstarget/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.AMirConsTarget.as_view(), name="mirconstarget"),
     url(r'results', views.result, name='amirconstarget'),
+    url(r'^', views.AMirConsTarget.as_view(), name="mirconstarget"),
+
     #url(r'^/*$', views.),
     #url(r'^/*$', views.input),
     #url(r'run$', views.run),
