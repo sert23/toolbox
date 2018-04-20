99fc38eb2b5340c052109b9ae04efc7805eae8ec
eaparicio45 <erapariciopuerta@hotmail.com>
2018-04-20 13:00:40 +0200

    include SUB_SITE in static folder

--- sRNAtoolboxweb/sRNAtoolboxweb/settings.py
+++ sRNAtoolboxweb/sRNAtoolboxweb/settings.py
@@ -155,7 +155,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+SUB_SITE = '/srnatoolbox_dev'
+STATIC_URL = os.path.join(SUB_SITE,'/static/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sRNAtoolboxweb', 'static'),
     MEDIA_ROOT
