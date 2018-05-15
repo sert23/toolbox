0eb7e8bc38bb3645cef91e0c08ad47898e79fe96
eaparicio45 <erapariciopuerta@hotmail.com>
2018-05-15 16:16:18 +0200

    fixing helper views

--- sRNAtoolboxweb/helpers/views.py
+++ sRNAtoolboxweb/helpers/views.py
@@ -264,6 +264,11 @@ class Extract(FormView):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         pipeline_id,call = form.create_call()
+        os.system(call)
+        js = JobStatus.objects.get(pipeline_key=pipeline_id)
+        js.status.create(status_progress='sent_to_queue')
+        js.job_status = 'sent_to_queue'
+        js.save()
         self.success_url = reverse_lazy('helper') + '?id=' + pipeline_id
         return super(Extract, self).form_valid(form)
 
