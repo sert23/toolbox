9f612195dfd1acde23067440eec2067620c13e9a
eaparicio45 <erapariciopuerta@hotmail.com>
2018-06-06 13:44:03 +0200

    changed SUB_SITE

--- sRNAtoolboxweb/sRNAtoolboxweb/settings.py
+++ sRNAtoolboxweb/sRNAtoolboxweb/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # SUB_SITE = ''
-SUB_SITE = '/srnatoolbox_dev'
+SUB_SITE = '/srnatoolbox'
 STATIC_URL = SUB_SITE + '/static/'
 MEDIA_URL = SUB_SITE + '/media/'
 
