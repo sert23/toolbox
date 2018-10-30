7aa43586703305b53ee656c02bc122546a1d25b4
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-30 18:47:06 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/urls.py
+++ sRNAtoolboxweb/photos/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.give_ID , name='multi_start'),
+    url(r'^$', views.give_ID.as_view() , name='multi_start'),
+    # url(r'^$', views.give_ID , name='multi_start'),
     url(r'^meme', ProgressBarUploadView.as_view(), name='progress_bar_upload'),
     # url(r'^$', views.ProgressBarUploadView.as_view() , name='multi_start'),
     url(r'^clear/$', views.clear_database, name='clear_database'),
