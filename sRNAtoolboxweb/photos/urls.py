6652927f3c8b5b323557e8d664e6cb5c62485957
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-30 20:45:55 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/urls.py
+++ sRNAtoolboxweb/photos/urls.py
@@ -6,11 +6,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.GetIDView.as_view() , name='multi_start'),
     # url(r'^$', views.give_ID , name='multi_start'),
-    url(r'^[A-za-z0-9]+', ProgressBarUploadView.as_view()),
+    url(r'^[A-za-z0-9]+', views.ProgressBarUploadView.as_view()),
     # url(r'^$', views.ProgressBarUploadView.as_view() , name='multi_start'),
     url(r'^clear/$', views.clear_database, name='clear_database'),
-    url(r'^$', Bench.as_view(), name="lolo"),
-    # url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
+    # url(r'^$', Bench.as_view(), name="lolo"),
+    ## url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
     # url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
-    url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+    # url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
 ]
