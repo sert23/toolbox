941540e8cb17df35fa88bf6ef21c68fa973162db
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-30 20:53:00 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/urls.py
+++ sRNAtoolboxweb/photos/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     # url(r'^$', Bench.as_view(), name="lolo"),
     ## url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
     # url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
-    # url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+    url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
 ]
