fffb5d74e8fd60ae08eee7d78d10c8194e7a5de6
eaparicio45 <erapariciopuerta@hotmail.com>
2018-10-30 23:59:34 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/models.py
+++ sRNAtoolboxweb/photos/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='photos/')
+    file = models.FileField(upload_to='photos2/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
