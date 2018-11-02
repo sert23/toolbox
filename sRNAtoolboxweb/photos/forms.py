748939551ddb071743d9a12fb772054f2b7d66aa
eaparicio45 <erapariciopuerta@hotmail.com>
2018-11-02 10:56:10 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/forms.py
+++ sRNAtoolboxweb/photos/forms.py
@@ -4,22 +4,22 @@ from .models import Photo2
 from django.db import models
 
 
-class PhotoForm_old(forms.ModelForm):
+class PhotoForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         if kwargs.get("request_path"):
             self.request_path = kwargs.pop("request_path", None)
-        super(PhotoForm_old, self).__init__(*args, **kwargs)
+        super(PhotoForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = Photo2
         fields = ('file', )
 
 
-class PhotoForm(forms.ModelForm):
-    # def __init__(self, *args, **kwargs):
-    #     self.request_path = kwargs.pop("request_path", None)
-    #     super(PhotoForm, self).__init__(*args, **kwargs)
-
-    #title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to= '%Y%m%d%H%M')
-    #uploaded_at = models.DateTimeField(auto_now_add=True)
+# class PhotoForm(forms.ModelForm):
+#     # def __init__(self, *args, **kwargs):
+#     #     self.request_path = kwargs.pop("request_path", None)
+#     #     super(PhotoForm, self).__init__(*args, **kwargs)
+#
+#     #title = models.CharField(max_length=255, blank=True)
+#     file = models.FileField(upload_to= '%Y%m%d%H%M')
+#     #uploaded_at = models.DateTimeField(auto_now_add=True)
