e20f9073f5810f82299f7bae7a220b51c0d3526d
eaparicio45 <erapariciopuerta@hotmail.com>
2018-11-05 15:40:15 +0100

    fresh start photos

--- sRNAtoolboxweb/sRNABench/forms.py
+++ sRNAtoolboxweb/sRNABench/forms.py
@@ -106,7 +106,7 @@ class sRNABenchForm(forms.Form):
 
     # MicroRNA Analysis
 
-    referenceDB = forms.ChoiceField(label="", choices=[("miRBase","Use miRBase (default)"),("highconf","Use only high confidence miRNAs from miRBase"),("MirGeneDB","Use mirGeneDB")], required=False, widget=forms.RadioSelect())
+    referenceDB = forms.ChoiceField(label="  ", choices=[("miRBase","Use miRBase (default)"),("highconf","Use only high confidence miRNAs from miRBase"),("MirGeneDB","Use mirGeneDB")], required=False, widget=forms.RadioSelect())
     genome_mir = forms.BooleanField(label='Use the miRNAs for the species from the selected genomes', required=False)
     highconf = forms.BooleanField(label='Use high confidence microRNAs from miRBase', required=False, initial=False)
     mirDB = forms.ChoiceField(label="Select MirGeneDBv2.0 tag", choices=mirdb_list, required=False)
