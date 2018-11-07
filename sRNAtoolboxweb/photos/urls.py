200fbb823180bd9a6500385e2407f5f7f8440e2c
eaparicio45 <erapariciopuerta@hotmail.com>
2018-11-07 17:25:16 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/urls.py
+++ sRNAtoolboxweb/photos/urls.py
@@ -10,14 +10,14 @@ urlpatterns = [
     # url(r'^/launch/[A-za-z0-9]+', views.MultiUploadView.as_view(), name='multi_launch'),
     #url(r'^(?P<query_id>[A-za-z0-9]+)', views.ProgressBarUploadView.as_view(),name='progress_bar_upload'),
     url(r'^[A-za-z0-9]+', views.MultiUploadView.as_view(),name='progress_bar_upload'),
-    url(r'^', views.MultiUploadView.as_view(), name='multi_start'),
+    #url(r'^', views.MultiUploadView.as_view(), name='multi_start'),
 
     # url(r'^$', views.give_ID , name='multi_start'),
 
     # url(r'^$', views.ProgressBarUploadView.as_view() , name='multi_start'),
-    url(r'^clear/$', views.clear_database, name='clear_database'),
+    #url(r'^clear/$', views.clear_database, name='clear_database'),
     # url(r'^$', Bench.as_view(), name="lolo"),
     ## url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
     # url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
-    url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
+    #url(r'^drag-and-drop-upload/$', views.DragAndDropUploadView.as_view(), name='drag_and_drop_upload'),
 ]
