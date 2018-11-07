e8923120fe858f5cbdee67ff4eb0bacc176c0c8c
eaparicio45 <erapariciopuerta@hotmail.com>
2018-11-07 17:23:16 +0100

    fresh start photos

--- sRNAtoolboxweb/photos/urls.py
+++ sRNAtoolboxweb/photos/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # url(r'^/launch/[A-za-z0-9]+', views.MultiUploadView.as_view(), name='multi_launch'),
     #url(r'^(?P<query_id>[A-za-z0-9]+)', views.ProgressBarUploadView.as_view(),name='progress_bar_upload'),
     url(r'^[A-za-z0-9]+', views.MultiUploadView.as_view(),name='progress_bar_upload'),
-    #url(r'^', views.MultiUploadView.as_view(), name='multi_start'),
+    url(r'^', views.MultiUploadView.as_view(), name='multi_start'),
 
     # url(r'^$', views.give_ID , name='multi_start'),
 
