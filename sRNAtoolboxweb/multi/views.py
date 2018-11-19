468cb75906c48fa61ca988f30fc04ed33b5bf0a4
eaparicio45 <erapariciopuerta@hotmail.com>
2018-11-19 16:39:08 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/multi/views.py
+++ sRNAtoolboxweb/multi/views.py
@@ -226,8 +226,8 @@ class MultiStatusView(DetailView):
             if job_stat == "Running":
                 context["running"] = True
             start = new_record.start_time.strftime("%H:%M, %d %b %Y")
-            finish = new_record.finish_time.strftime("%H:%M, %d %b %Y")
-
+            #finish = new_record.finish_time.strftime("%H:%M, %d %b %Y")
+            finish = str(new_record.finish_time)
             # job_stat = "sent_to_queue"
             click = '<a href="'+SUB_SITE+'/jobstatus/' + id +'" target="_blank" > Go to results </a>'
             jobs_tbody.append([job, job_stat, start, finish, click])
