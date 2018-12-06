2724a0e6d869b4fb59cb8f02f90dfdd3f3651117
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-06 21:28:04 +0100

    midway test for multilaunch

--- core/pipelines/tools/sRNAbenchPipeline.py
+++ core/pipelines/tools/sRNAbenchPipeline.py
@@ -5,16 +5,18 @@ from pipelines.pipelines import Pipeline
 #from progress.models import JobStatus
 import os
 import time
-
+import urllib.request
 
 class sRNAbenchPipeline(Pipeline):
     def __init__(self, pipeline_key, job_name, outdir, config_file=None, parameters=""):
         super(sRNAbenchPipeline, self).__init__(pipeline_key, job_name, outdir, "sRNAbench", parameters)
 
         self.conf = config_file
+        self.outdir = outdir
 
     def run(self):
         self.initialize_pipeline_status()
+        self.download_url()
         self.call_srnabench()
         self.set_finish_time()
         time.sleep(10)
@@ -24,6 +26,22 @@ class sRNAbenchPipeline(Pipeline):
             self.change_pipeline_status("Finished with Errors")
         # self.logger.close()
         self.error_logger.close()
+
+    def download_url(self):
+        with open(self.conf, "r") as  config_r:
+            lines = config_r.readlines()
+        for line in lines:
+            if line.startswith("input=URL@"):
+                url= line.split("@")[1]
+                dest = os.path.join(self.outdir, os.path.basename(url))
+                ifile, headers = urllib.request.urlretrieve(url, filename=dest)
+                new_line ="input="+dest+"\n"
+        with open(self.conf, "w") as  config_w:
+            for line in lines:
+                if line.startswith("input=URL@"):
+                    config_w.write(new_line)
+                else:
+                    config_w.write(line)
 
     def call_srnabench(self):
         log_msg = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " INFO: sRNAbench Analysis Starts"
