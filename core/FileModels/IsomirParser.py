e8c1c3ccc3735f384b7d9e0564ab9f56af384b2c
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-06 18:19:40 +0100

    midway test for multilaunch

--- core/FileModels/IsomirParser.py
+++ core/FileModels/IsomirParser.py
@@ -7,7 +7,9 @@ class IsomirParser(Parser):
     def parse(self):
         fd = open(self.ipath)
         header = fd.readline().replace("\n", "").split("\t")
+        header = header + ["Draw"]
         for line in fd:
             aline = line.replace("\n", "").split("\t")
+            aline = aline + [aline[0]]
             yield Isomir(*aline)
         fd.close()
