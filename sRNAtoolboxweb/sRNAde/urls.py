0af4fe3c06026046408803bd967d4d3531200955
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-19 00:45:40 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/sRNAde/urls.py
+++ sRNAtoolboxweb/sRNAde/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 urlpatterns = [
     #url(r'^ncbiparser', views.NCBI.as_view(), name="ncbi"),
+    url(r'result', views.result, name='srnade'),
     url(r'^$', views.De.as_view(), name="DE"),
     url(r'launch/(?P<pipeline_id>[A-za-z0-9]+)', views.DeLaunch.as_view()),
     url(r'launch/', views.DeLaunch.as_view(), name="DE_launch"),
     # url(r'^/*$', views.de),
     # url(r'run$', views.run, name='run_de'),
-    url(r'result', views.result, name='srnade'),
+
     url(r'test$', views.test),
 
 ]
