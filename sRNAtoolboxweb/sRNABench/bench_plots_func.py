9c9e98ffc5fd35d890613d877ad4b7a4cfa4e6b3
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-21 16:08:09 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/sRNABench/bench_plots_func.py
+++ sRNAtoolboxweb/sRNABench/bench_plots_func.py
@@ -16,12 +16,13 @@ def Full_read_length_divs(input_folder, generate=True):
     out_path1 = os.path.join(input_folder,"stat","readLength_RC.png")
     out_path2 = os.path.join(input_folder,"stat","readLength_UR.png")
 
-    os.mkdir(os.path.join(input_folder, "stat", "1"))
+    #os.mkdir(os.path.join(input_folder, "stat", "1"))
 
     if not os.path.exists(out_path1):
         os.mkdir(os.path.join(input_folder,"stat","2"))
         subprocess.Popen([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder])
-        with open(os.path.join(input_folder,"stat","test.out", "w")) as test_f:
+        os.system("touch " + os.path.join(input_folder,"stat","2","test.out", "w"))
+        with open(os.path.join(input_folder,"stat","2","test.out", "w")) as test_f:
             test_f.write(" ".join([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder]))
 
     input_table = pandas.read_table(length_file, sep='\t')
