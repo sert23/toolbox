1525835f1f03e5d1b89ad0f4eb713d5088cd3725
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-22 20:24:21 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/sRNABench/bench_plots_func.py
+++ sRNAtoolboxweb/sRNABench/bench_plots_func.py
@@ -45,7 +45,7 @@ def Full_read_length_divs(input_folder, generate=True):
         ),
         title="Read length distribution ",
         autosize=False,
-        height=600,
+        height=650,
         width=1150,
         xaxis=dict(
             title='Read length (nt)',
