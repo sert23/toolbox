5957fb13e9b58457c69a363146969d2ee49f892b
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-23 01:41:50 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/sRNABench/bench_plots_func.py
+++ sRNAtoolboxweb/sRNABench/bench_plots_func.py
@@ -8,6 +8,7 @@ import plotly
 import plotly.io as pio
 from sRNAtoolboxweb.settings import MEDIA_ROOT,BENCH_PLOTLY,PATH_TO_VENV, MEDIA_URL
 import subprocess
+import sys
 
 
 def Full_read_length_divs(input_folder, png=False):
@@ -18,24 +19,33 @@ def Full_read_length_divs(input_folder, png=False):
 
     #os.mkdir(os.path.join(input_folder, "stat", "1"))
 
-    if False:
-        # os.system("touch " + os.path.join(input_folder, "stat", "2", "test.out"))
-        # os.mkdir(os.path.join(input_folder,"stat","2"))
-        #
-        call = " ".join([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder])
-        with open(os.path.join(input_folder,"stat","test.out"), "w") as test_f:
-            test_f.write(call)
-        #os.system(" ".join([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder]))
-        #subprocess.call('source ' +os.path.join(PATH_TO_VENV,"activate") + ';' + call , shell=True)
-        plotter = subprocess.Popen([os.path.join(PATH_TO_VENV,"python3.5"), BENCH_PLOTLY ,"readLength", input_folder],stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE)
-        # [outStream, errStream] = plotter.communicate()
-        # with open(os.path.join(input_folder,"stat","err.out"), "w") as test_f:
-        #     test_f.write(str(outStream))
-        #     test_f.write(str(errStream))
+    if (not os.path.exists(out_path1)) and (not png):
+        call_list = [os.path.join(PATH_TO_VENV, "python"), BENCH_PLOTLY, "readLength", input_folder]
+        with open(os.path.join(input_folder, "stat", "test.out"), "w") as test_f:
+            test_f.write(" ".join(call_list))
+        plotter = subprocess.Popen(call_list,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
 
-        # subprocess.Popen([os.path.join(PATH_TO_VENV,"python3"), BENCH_PLOTLY ,"readLength", input_folder])
-        # os.system("touch " + os.path.join(input_folder,"stat","2","test.out", "w"))
+    #
+    # if False:
+    #     # os.system("touch " + os.path.join(input_folder, "stat", "2", "test.out"))
+    #     # os.mkdir(os.path.join(input_folder,"stat","2"))
+    #     #
+    #     call = " ".join([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder])
+    #     with open(os.path.join(input_folder,"stat","test.out"), "w") as test_f:
+    #         test_f.write(call)
+    #     #os.system(" ".join([os.path.join(PATH_TO_VENV,"python"), BENCH_PLOTLY ,"readLength", input_folder]))
+    #     #subprocess.call('source ' +os.path.join(PATH_TO_VENV,"activate") + ';' + call , shell=True)
+    #     plotter = subprocess.Popen([os.path.join(PATH_TO_VENV,"python3.5"), BENCH_PLOTLY ,"readLength", input_folder],stdout=subprocess.PIPE,
+    #                                  stderr=subprocess.PIPE)
+    #     # [outStream, errStream] = plotter.communicate()
+    #     # with open(os.path.join(input_folder,"stat","err.out"), "w") as test_f:
+    #     #     test_f.write(str(outStream))
+    #     #     test_f.write(str(errStream))
+    #
+    #     # subprocess.Popen([os.path.join(PATH_TO_VENV,"python3"), BENCH_PLOTLY ,"readLength", input_folder])
+    #     # os.system("touch " + os.path.join(input_folder,"stat","2","test.out", "w"))
     # elif not os.path.exists(out_path1) :
 
     input_table = pandas.read_table(length_file, sep='\t')
@@ -67,9 +77,12 @@ def Full_read_length_divs(input_folder, png=False):
     fig = go.Figure(data=data, layout=layout)
     # py.image.save_as({'data': data}, 'your_image_filename.png')
     #plot(fig, filename=out_path1, show_link=False, auto_open=False)
-    div_obj1 = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
-    #div_obj1_b = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
+    if png:
+        plotly.io.write_image(fig, out_path1, format="png", width=None, height=None)
+    else:
+        div_obj1 = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
 
+    #div_obj1_b = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
 
     data = [go.Bar(x=x,y=y2)]
 
@@ -97,7 +110,11 @@ def Full_read_length_divs(input_folder, png=False):
 
     fig = go.Figure(data=data, layout=layout)
 
-    div_obj2 = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
+    if png:
+        plotly.io.write_image(fig, out_path2, format="png", width=None, height=None)
+    else:
+        div_obj2 = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
+    #div_obj2 = plot(fig, show_link=False, auto_open=False, output_type='div', include_plotlyjs=False)
     # plot({'data': [{'y': [4, 2, 3, 4]}],
     #               'layout': {'title': 'Test Plot',
     #                          'font': dict(family='Comic Sans MS', size=16)}},
@@ -105,13 +122,14 @@ def Full_read_length_divs(input_folder, png=False):
     #              output_type='file', image_width=800, image_height=600,
     #              filename='temp-plot.html', validate=False)
 
-    out_path1 = out_path1.replace(MEDIA_ROOT,MEDIA_URL)
-    out_path2 = out_path2.replace(MEDIA_ROOT,MEDIA_URL)
-    id1 = div_obj1.split("\"")[1]
-    #id1_b = div_obj1_b.split("\"")[1]
-    id2 = div_obj2.split("\"")[1]
+    if not png:
+        out_path1 = out_path1.replace(MEDIA_ROOT,MEDIA_URL)
+        out_path2 = out_path2.replace(MEDIA_ROOT,MEDIA_URL)
+        id1 = div_obj1.split("\"")[1]
+        #id1_b = div_obj1_b.split("\"")[1]
+        id2 = div_obj2.split("\"")[1]
 
-    return [[div_obj1 ,out_path1, id1, "img_"+id1],[div_obj2, out_path2, id2, "img_"+id2]]
+        return [[div_obj1 ,out_path1, id1, "img_"+id1],[div_obj2, out_path2, id2, "img_"+id2]]
     # return [[div_obj2, out_path2, id2, "img_"+id2]]
     #return [[div_obj1 ,out_path1, id1, "img_"+id1],[div_obj1_b ,out_path1, id1_b, "img_"+id1_b]]
 
@@ -141,20 +159,8 @@ def Full_read_length_divs(input_folder, png=False):
 # print(plotly.__version__)
 #Full_read_length("C:/Users/Ernesto/Desktop/Colabo/Dani_platelets/")
 
-def stest_ml():
-    from plotly.offline import init_notebook_mode, plot_mpl
-    import matplotlib.pyplot as plt
+p_type = sys.argv[1]
+input_par = sys.argv[2]
 
-    #init_notebook_mode()
-
-    fig = plt.figure()
-    x = [10, 15, 20, 25, 30]
-    y = [100, 250, 200, 150, 300]
-    plt.plot(x, y, "o")
-
-    plot_mpl(fig)
-    # If you want to to download an image of the figure as well
-    plot_mpl(fig, image='png',image_filename='plot_image')
-
-#stest_ml()
-
+if p_type == "readLength":
+    Full_read_length_divs(input_par, png=True )
