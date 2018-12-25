e06be4712a0f03b04f05451a41c250d2ecdd747f
eaparicio <erapariciopuerta@gmail.com>
2018-12-26 00:55:18 +0100

    adding multistatus tepmlate

--- sRNAtoolboxweb/sRNABench/bench_plots_gen.py
+++ sRNAtoolboxweb/sRNABench/bench_plots_gen.py
@@ -166,8 +166,8 @@ def Read_len_type(input_folder, path_to_venv=None, plotly_script=None, media_url
     length_file = os.path.join(input_folder, "stat", "rnaComposition_readLength_sensePref.txt")
     out_path = os.path.join(input_folder, "stat", "rrnaComposition_readLength.png")
 
-    if False:
-    # if (not os.path.exists(out_path)) and (not png):
+    # if False:
+    if (not os.path.exists(out_path)) and (not png):
         call_list = [os.path.join(path_to_venv, "python"), plotly_script, "lenType", input_folder]
         with open(os.path.join(input_folder, "stat", "test.out"), "w") as test_f:
             test_f.write(" ".join(call_list))
