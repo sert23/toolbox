7c0d4b2a311b5f962cd1f371bed69391e4b452f3
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-28 13:57:18 +0100

    midway test for multilaunch

---
+++ sRNAtoolboxweb/FileModels/GeneralParser.py
@@ -0,0 +1,13 @@
+from DataModels.generalTable import General
+from FileModels.Parser import Parser
+
+__author__ = 'antonior'
+
+class GeneralParser(Parser):
+    def parse(self):
+        fd = open(self.ipath)
+        header = fd.readline().replace("\n", "").split("\t")
+        for line in fd:
+            aline = line.replace("\n", "").split("\t")
+            yield General(*aline)
+        fd.close()
