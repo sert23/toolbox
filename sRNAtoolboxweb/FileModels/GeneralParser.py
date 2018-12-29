5c7a9fe0a9425221d7d9eb7196dd84b6b472ea38
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-29 22:19:10 +0100

    David's stuff

--- sRNAtoolboxweb/FileModels/GeneralParser.py
+++ sRNAtoolboxweb/FileModels/GeneralParser.py
@@ -3,11 +3,15 @@ from FileModels.Parser import Parser
 
 __author__ = 'antonior'
 
+
 class GeneralParser(Parser):
     def parse(self):
         fd = open(self.ipath)
         header = fd.readline().replace("\n", "").split("\t")
         for line in fd:
             aline = line.replace("\n", "").split("\t")
+            numbers = aline[1:]
+            numbers = [float(x) for x in numbers]
+            aline = [aline[0]] + [round(x, 2) for x in numbers]
             yield General(header, *aline)
         fd.close()
