a99f7075c99c07719f8c40a3df4860d054a69892
eaparicio45 <erapariciopuerta@hotmail.com>
2018-12-30 02:21:18 +0100

    midway test for multilaunch

--- sRNAtoolboxweb/FileModels/GeneralParser.py
+++ sRNAtoolboxweb/FileModels/GeneralParser.py
@@ -4,10 +4,13 @@ from FileModels.Parser import Parser
 __author__ = 'antonior'
 
 
-def is_numeric(s):
+def is_dec(s):
     try:
-        float(s)
-        return True
+        c = float(s)
+        if c.is_integer():
+            return False
+        else:
+            return True
     except ValueError:
         return False
 
@@ -17,6 +20,6 @@ class GeneralParser(Parser):
         header = fd.readline().replace("\n", "").split("\t")
         for line in fd:
             aline = line.replace("\n", "").split("\t")
-            aline = [round(float(x),2) if is_numeric(x) else x for x in aline]
+            aline = [round(float(x),2) if is_dec(x) else x for x in aline]
             yield General(header, *aline)
         fd.close()
