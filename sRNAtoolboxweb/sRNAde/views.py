ac530194ab6cb5e2816d58bc3c3b3cf01825672a
eaparicio <erapariciopuerta@gmail.com>
2019-02-21 18:12:31 +0100

    add

--- sRNAtoolboxweb/sRNAde/views.py
+++ sRNAtoolboxweb/sRNAde/views.py
@@ -482,7 +482,8 @@ class De_method_view(DetailView):
             with open(os.path.join(folder,"heatmap.config"),"r") as multi_f:
                 for line in multi_f.readlines():
                     input_path, title, tag = line.rstrip().split("\t")
-                    plot = file2string(input_path)
+                    plot='<iframe src="'+ input_path.replace(MEDIA_ROOT,MEDIA_URL)  +'"></iframe>'
+                    #plot = file2string(input_path)
                     # plot = multiBP(input_path, title=title, xlab=xlab, ylab=ylab)
                     mbp_list.append([plot,sections_dic[tag]])
 
