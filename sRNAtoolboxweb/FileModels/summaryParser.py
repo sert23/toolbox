f29084b92b424565a3a87816b0d10619f9ba9cba
eaparicio <erapariciopuerta@gmail.com>
2019-03-01 08:21:38 +0200

    computing BigWig table

--- sRNAtoolboxweb/FileModels/summaryParser.py
+++ sRNAtoolboxweb/FileModels/summaryParser.py
@@ -30,3 +30,17 @@ class LinksParser(Parser):
             # aline = aline[:-1] + ['<a href="'+link+'" class="btn btn-primary btn-sm" role="button" aria-pressed="true">Go to results</a>']
             yield General(header, *aline)
         fd.close()
+
+class BWParser(Parser):
+    def parse(self, MEDIA_ROOT, MEDIA_URL):
+        fd = open(self.ipath)
+        header = ["description","link"]
+        #header = header[0:-1] + ["Link to results"]
+        for line in fd:
+            aline = line.replace("\n", "").split("\t")
+            media_path = aline[-1].replace(MEDIA_ROOT,MEDIA_URL)
+            link = media_path
+            aline = aline[:-1] + [link]
+            # aline = aline[:-1] + ['<a href="'+link+'" class="btn btn-primary btn-sm" role="button" aria-pressed="true">Go to results</a>']
+            yield General(header, *aline)
+        fd.close()
