f6a3c4423960864abf7d40c3f0f4754469d1deaa
eaparicio <erapariciopuerta@gmail.com>
2019-05-17 15:01:17 +0200

    fixing spacing

--- sRNAtoolboxweb/helpers/views.py
+++ sRNAtoolboxweb/helpers/views.py
@@ -235,7 +235,7 @@ def result(request):
                     value = line.replace("\n", "").split(" ")[-1]
                     backvalue = value
                 if "INFO" in line or "SUCCESS" in line:
-                    info_string += line
+                    info_string += line + "\n"
 
             zip_file = os.path.join(backvalue + ".zip").split("/")[-1]
             if os.path.exists(backvalue+".zip"):
