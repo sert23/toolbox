0fc214be682783ca958887e9e0ea75a42ee4543f
eaparicio <erapariciopuerta@gmail.com>
2019-05-23 01:08:13 +0200

    fixing new queue for helpers

--- sRNAtoolboxweb/helpers/forms.py
+++ sRNAtoolboxweb/helpers/forms.py
@@ -301,7 +301,7 @@ class ExtractForm(forms.Form):
                                  outdir=out_dir,
                                 )
         if QSUB:
-            return 'qsub -q ff -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
+            return 'qsub -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
                 configuration_file_path=configuration_file_path,
                 job_name=name,
                 sh=os.path.join(os.path.dirname(BASE_DIR) + '/core/bash_scripts/run_qsub.sh')), pipeline_id
@@ -391,7 +391,7 @@ class EnsemblForm(forms.Form):
                                 )
 
         if QSUB:
-            return 'qsub -q ff -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
+            return 'qsub -q fast -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
                 configuration_file_path=configuration_file_path,
                 job_name=name,
                 sh=os.path.join(os.path.dirname(BASE_DIR) + '/core/bash_scripts/run_qsub.sh')), pipeline_id
@@ -479,7 +479,7 @@ class NcbiForm(forms.Form):
                                  outdir=FS.location,
                                  )
         if QSUB:
-            return 'qsub -q ff -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
+            return 'qsub -q fast -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
                 configuration_file_path=configuration_file_path,
                 job_name=name,
                 sh=os.path.join(os.path.dirname(BASE_DIR) + '/core/bash_scripts/run_qsub.sh')), pipeline_id
@@ -567,7 +567,7 @@ class RnacentralForm(forms.Form):
                                  outdir=FS.location,
                                  )
         if QSUB:
-            return 'qsub -q ff -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
+            return 'qsub -q fast -v c="{configuration_file_path}" -N {job_name} {sh}'.format(
                 configuration_file_path=configuration_file_path,
                 job_name=name,
                 sh=os.path.join(os.path.dirname(BASE_DIR) + '/core/bash_scripts/run_qsub.sh')), pipeline_id
