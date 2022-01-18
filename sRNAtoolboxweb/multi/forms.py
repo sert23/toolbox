66a08cfa8866932fdb7139868d8828dc4d0afe4d
eaparicio <erapariciopuerta@gmail.com>
2022-01-18 11:09:17 +0100

    uploaded files not working

--- sRNAtoolboxweb/multi/forms.py
+++ sRNAtoolboxweb/multi/forms.py
@@ -597,11 +597,11 @@ class sRNABenchForm(forms.Form):
             elif an_object["input_type"] == "uploaded file":
                 input_file = an_object["name"]
                 dest_path = os.path.join(MEDIA_ROOT, new_id, input_file)
-                shutil.copyfile(os.path.join(MEDIA_ROOT, pipeline_id, "files", input_file), dest_path)
+                shutil.copyfile(os.path.join(MEDIA_ROOT, self.folder, "files", input_file), dest_path)
             elif an_object["input_type"] == "Drive":
                 input_file = an_object["name"]
                 dest_path = os.path.join(MEDIA_ROOT, new_id, input_file)
-                shutil.copyfile(os.path.join(MEDIA_ROOT, pipeline_id, "files", "drive_temp", input_file), dest_path)
+                shutil.copyfile(os.path.join(MEDIA_ROOT, self.folder, "files", "drive_temp", input_file), dest_path)
 
             line = "input=" + dest_path + "\n"
             line2 = "output=" + out_dir + "\n"
