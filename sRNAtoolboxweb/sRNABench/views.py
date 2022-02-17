dbfab417612c5d455d04abe3c5f6a4d56d3c1bb1
eaparicio <erapariciopuerta@gmail.com>
2022-02-17 16:58:00 +0100

    .

--- sRNAtoolboxweb/sRNABench/views.py
+++ sRNAtoolboxweb/sRNABench/views.py
@@ -937,7 +937,7 @@ def show_align(request, job_id, type, name):
             elif os.path.exists(afile):
                 content = open(afile).readlines()
             else:
-                content = "No Alignment available. Read count below threshold" + afile
+                content = "No Alignment available. Read count below threshold" + afile + short_name +" test" + name
             desc = "alignment to pre-microRNA"
             back = "pre-microRNA"
 
