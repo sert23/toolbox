ccdd4ed34ee2027e82e5f9fb78e295ffd54fe316
eaparicio <erapariciopuerta@gmail.com>
2022-03-13 01:11:04 +0100

    .

--- sRNAtoolboxweb/newbench/urls.py
+++ sRNAtoolboxweb/newbench/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     url(r'^annotate/', views.Launch.as_view(), name="annotate"),
     url(r'^relaunch', views.ReLaunch.as_view(), name="newbench_relaunch"),
     url(r'^matrix_selector', views.ajax_matrix_selectors, name="ajax_matrix_selector"),
-    url(r'^matrix_generator', views.ajax_matrix_selectors, name="matrix_generator"),
+    url(r'^matrix_generator', views.matrix_generator, name="matrix_generator"),
     # url(r'^launch/[A-za-z0-9]+', views.NewUpload.as_view()),
     #url(r'test$', views.test),
 ]
