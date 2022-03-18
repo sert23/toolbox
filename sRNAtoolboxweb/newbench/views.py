f34c5582dd0a32063cffae36bab0a7064453eb91
eaparicio <erapariciopuerta@gmail.com>
2022-03-18 14:13:31 +0100

    .

--- sRNAtoolboxweb/newbench/views.py
+++ sRNAtoolboxweb/newbench/views.py
@@ -559,7 +559,7 @@ def matrix_generator(request):
         print("x")
     else:
         # try:
-        grpString, names = make_grpStr(jobID)
+        grpString = make_grpStr(jobID)
         line = "java -jar " + jar_file + " " + params + " colData={column} statFiles={annot_file} input={base_folder} grpString={jobs} output={output_folder}"
         command_line = line.format(column=column,
                                    annot_file=annot_file,
